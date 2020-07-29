4210b6d9f7c065d49e369f7db3b4895321cb3f15
Tom Austin <Tom.Austin@streets-heaver.com>
2020-07-29 20:17:09 +0100

    Add project files.

---
+++ ElasticSeries/SeriesClient.cs
@@ -0,0 +1,14 @@
+﻿using Nest;
+using System;
+
+namespace ElasticSeries
+{
+    public class SeriesClient
+    {
+        private readonly ElasticClient _elasticClient;
+        public SeriesClient(ConnectionSettings settings)
+        {
+            _elasticClient = new ElasticClient(settings);
+        }
+    }
+}
